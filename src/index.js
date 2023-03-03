6239f7e35f979feb5b4a81e70b7163f419126790
henriquesdias <henrique.dias1915@gmail.com>
2023-03-03 12:04:44 -0300

    feat: resolves problem with cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import http from "http";
 
 const article = [];
+let id = 0;
 
 http
-  .createServer(function (req, res) {
+  .createServer((req, res) => {
     const completeUrl = req.url;
     const url = completeUrl.split("?")[0];
     const queryString = completeUrl.split("?")[1];
     const method = req.method;
     const chunks = [];
-
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS, PUT, POST, GET",
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "Acess-Control-Max-Age": 86400,
+    };
+    if (req.method === "OPTIONS") {
+      res.writeHead(204, headers);
+      res.end();
+      return;
+    }
     if (url === "/api/getArticle" && method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
+      res.writeHead(200, headers);
       res.write(JSON.stringify(article));
       res.end();
       return;
@@ -24,15 +36,17 @@ http
       req.on("end", () => {
         const data = Buffer.concat(chunks);
         const dataArticle = data.toString();
-        article.push(JSON.parse(dataArticle));
-        res.writeHead(201, { "Content-Type": "json" });
+        article.push({ ...JSON.parse(dataArticle), id });
+        id++;
+        res.writeHead(200, headers);
         res.write(JSON.stringify(article));
+        console.log(article);
         res.end();
       });
       return;
     }
 
-    res.writeHead(400);
+    res.writeHead(400, headers);
     res.write("Error");
     res.end();
   })
